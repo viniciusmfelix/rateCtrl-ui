527e94c68c806e14631240108e38c3c28113b857
viniciusmfelix <vinicius.mfelix@icloud.com>
2021-01-21 18:35:50 -0300

    Adição de rotas do componente de menu e colaborador

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ColaboradorComponent } from './components/colaborador/colaborador.component';
+import { MenuComponent } from './components/menu/menu.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: MenuComponent,
+    children: [
+      {
+        path: 'colaboradores',
+        component: ColaboradorComponent
+      },
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
